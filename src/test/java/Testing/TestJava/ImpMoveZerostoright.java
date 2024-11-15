589c96e6a81b8183be90c9b4f5c9a0fd923dfb9e
Prasanthpsg <68192271+Prasanthpsg@users.noreply.github.com>
2024-11-15 13:16:09 +0530

    Update ImpMoveZerostoright.java

--- src/test/java/Testing/TestJava/ImpMoveZerostoright.java
+++ src/test/java/Testing/TestJava/ImpMoveZerostoright.java
@@ -28,6 +28,23 @@ public class ImpMoveZerostoright {
 			System.out.print(b[i]+" ");
 		}
 
+		// sapient answer
+		  int[] arr = {2,0,12,23,12,0,44,98,0,122,-223};
+       //shift to right without temp array
+       int t=0;
+       
+       for(int i=0;i<arr.length;i++){
+           if(arr[i]!=0){
+               arr[t++]=arr[i];
+           }
+       }
+       
+       while(arr.length>t){
+            arr[t++]=0;
+       }
+       
+       System.out.println(Arrays.toString(arr));
+
 	}
 
 }
