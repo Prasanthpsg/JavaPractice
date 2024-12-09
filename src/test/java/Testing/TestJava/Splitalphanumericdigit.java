cf88bd3e55cbea27d65f0dc9169eb6fd7861c0cd
Prasanthpsg <68192271+Prasanthpsg@users.noreply.github.com>
2024-12-09 23:17:51 +0530

    Good programs added

--- src/test/java/Testing/TestJava/Splitalphanumericdigit.java
+++ src/test/java/Testing/TestJava/Splitalphanumericdigit.java
@@ -27,3 +27,74 @@ public class Splitalphanumericdigit {
 	}
 
 }
+
+// print the repeated characters count a3b3acfw3dv2s
+class Main {
+    public static void main(String[] args) {
+      String str = "aaabbbacfwwwdvvs";
+      char[] c = str.toCharArray();
+		StringBuffer sb = new StringBuffer();
+		int count =1;int j=0;
+		
+		for(int i=0;i<c.length;i++){
+		     count =1;
+		    for( j=i+1;j<c.length;j++){
+		        if(c[i]==c[j]){
+		            count++;
+		        }else{
+		            i=j-1;
+		            break;
+		        }
+		    }
+		    if(count>1 ){
+		        sb.append(c[i]).append(count);
+		    }else{
+		        sb.append(c[i]);
+		    }
+		    
+		    if(j==c.length){
+		         System.out.println(c.length);
+		        break;
+		    }
+		    }
+		    
+		    System.out.println(sb.toString().trim());//a3b3acfw3dv2s
+		}
+		
+		
+}
+
+// reverse the characters alone in the given string
+
+public class Check {
+
+    public static void main(String[] args) {
+        String str = "33prasanth23kayal5pd";
+        char[] c = str.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        StringBuffer sb1 = new StringBuffer();
+        StringBuffer sb2 = new StringBuffer();
+
+        for(int i=0;i<c.length-1;i++){
+            if(Character.isDigit(c[i])){
+                sb.append(c[i]);
+            }else{
+                for(int j=i;j<c.length;j++){
+                    if(Character.isAlphabetic(c[j])){
+                        sb1.append(c[j]);
+                    }else{
+                        i=j-1;
+                        break;
+                    }
+                }
+                sb1.reverse();
+            }
+
+            sb2.append(sb).append(sb1);
+            sb.delete(0, sb.length());
+            sb1.delete(0, sb1.length());
+        }
+
+        System.out.println(sb2.toString().trim()); //33htnasarp23layak5dp
+    }
+}
